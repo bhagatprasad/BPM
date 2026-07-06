7a2da0e2bc9bc70c03657d23abf8bdffb4d18fb4
Surbhi Jambhulkar <Surbhi.Jambhulkar@betalen.in>
2026-07-06 14:14:03 +0530

    RoleService

---
+++ BPM.Web.API/Models/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BPM.Web.API.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BPM.Web.API.Models.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Role> Roles { get; set; }
+    }
+}
+
---
+++ BPM.Web.API/Repository/RoleRepository.cs
@@ -0,0 +1,65 @@
+﻿using BPM.Web.API.Models.Data;
+using BPM.Web.API.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BPM.Web.API.Repository
+{
+    public class RoleRepository : IRoleRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RoleRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Role>> GetAllRolesAsync()
+        {
+            return await _context.Roles
+                .OrderBy(x => x.Name)
+                .ToListAsync();
+        }
+
+        public async Task<Role?> GetRoleByIdAsync(Guid roleId)
+        {
+            return await _context.Roles
+                .FirstOrDefaultAsync(x => x.Id == roleId);
+        }
+
+        public async Task<bool> InsertRoleAsync(Role role)
+        {
+            role.CreatedOn = DateTime.UtcNow;
+            role.IsActive = true;
+
+            _context.Roles.Add(role);
+
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> UpdateRoleAsync(Role role)
+        {
+            var existingRole = await _context.Roles.FindAsync(role.Id);
+
+            if (existingRole == null)
+                return false;
+
+            existingRole.Name = role.Name;
+            existingRole.IsActive = role.IsActive;
+            existingRole.ModifiedOn = DateTime.UtcNow;
+
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> DeleteRoleAsync(Guid roleId)
+        {
+            var role = await _context.Roles.FindAsync(roleId);
+
+            if (role == null)
+                return false;
+
+            _context.Roles.Remove(role);
+
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
