6ce604ad5d04c3dfaeede61b9519e5becb3a33cd
soyamraghu <soyamraghu65@gmail.com>
2026-07-13 18:02:31 +0530

    GlobalExceptionHandler_Integration

---
+++ BPM.Web.API/GlobalExceptionHandling/ErrorDetails.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+
+namespace BPM.Web.API.GlobalExceptionHandling
+{
+    public class ErrorDetails
+    {
+        public int StatusCode { get; set; }
+        public string? Message { get; set; }
+
+        // Overriding ToString() to automatically serialize the object into JSON
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
---
+++ BPM.Web.API/GlobalExceptionHandling/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using System.Net;
+using System.Text.Json; 
+
+namespace BPM.Web.API.GlobalExceptionHandling
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static void ConfigureExceptionHandler(this WebApplication app, ILogger logger)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null)
+                    {
+                        
+                        logger.LogError($"Something went wrong: {contextFeature.Error}");
+
+                       
+                        var errorResponse = new 
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = "Internal Server Error. Please try again later."
+                        };
+
+                        
+                        var jsonString = JsonSerializer.Serialize(errorResponse);
+                        await context.Response.WriteAsync(jsonString);
+                    }
+                });
+            });
+        }
+    }
+}
