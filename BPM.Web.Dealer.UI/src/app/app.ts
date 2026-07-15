a0b5921fe25f48e8de60564251bc3562ceedce9e
bhagatprasad <bhagatprasad8@yahoo.com>
2026-07-15 09:08:39 +0530

    angular project setup

--- BPM.Web.Dealer.UI/src/app/app.ts
+++ BPM.Web.Dealer.UI/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidenavComponent } from './common/sidenav';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidenavComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
