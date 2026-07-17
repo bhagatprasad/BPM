44c6830bf8a4ec6a1609f6a7d431620b07f39bc6
kolipakarakesh <rakeshkolipaka8466@zohomail.in>
2026-07-17 15:55:21 +0530

    Implement add to cart functionality with dynamic cart count

--- BPM.Web.Dealer.UI/src/app/app.ts
+++ BPM.Web.Dealer.UI/src/app/app.ts
@@ -1,14 +1,23 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidenavComponent } from './common/sidenav';
-
+import { CartService } from './services/cart.service';
+import { count } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, SidenavComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('BPM.Web.Dealer.UI');
+  cartCount: number = 0;
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.cartService.cartCount$.subscribe((count) => {
+      this.cartCount = count;
+    });
+  }
 }
--- BPM.Web.Dealer.UI/src/app/guards/authentication-guard.ts
+++ BPM.Web.Dealer.UI/src/app/guards/authentication-guard.ts
@@ -2,12 +2,16 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
 export const authenticationGuard: CanActivateFn = (route, state) => {
-
-const router = inject(Router);
-  const loggeddata= localStorage.getItem('user');
+  const router = inject(Router);
+  const loggeddata = localStorage.getItem('user');
   if (!loggeddata) {
     router.navigateByUrl('login');
     return false;
   }
   return true;
 };
+// import { CanActivateFn } from '@angular/router';
+
+// export const authenticationGuard: CanActivateFn = () => {
+//   return true;
+// };
