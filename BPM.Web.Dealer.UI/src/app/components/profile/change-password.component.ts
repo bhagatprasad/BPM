98c58a0e690f9daeeb2abd8c8982562e50701509
soyamraghu <soyamraghu65@gmail.com>
2026-08-04 15:04:17 +0530

    User_Persional_Component_And_ChangePassword_Component_And_Dealer_Info_Components_Are_Integrated_Into_Profile_Component

--- BPM.Web.Dealer.UI/src/app/components/profile/change-password.component.ts
+++ BPM.Web.Dealer.UI/src/app/components/profile/change-password.component.ts
@@ -1,9 +1,93 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output, SimpleChanges } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-change-password',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './change-password.component.html',
   styleUrl: './change-password.component.css',
+  
 })
-export class ChangePasswordComponent {}
+export class ChangePasswordComponent {
+
+@Input() changePswd: any = {};
+@Input() isChangePassword: boolean = false;
+@Input() showPassword: boolean = false;
+@Input() showConfirmPassword: boolean = false;
+@Input() errorMsz: string = '';
+
+@Output() Edit = new EventEmitter<void>();
+@Output() togglePassword = new EventEmitter<void>();
+@Output() toggleConfirmPassword= new EventEmitter<void>();
+@Output() save = new EventEmitter<void>();
+@Output() cancel = new EventEmitter<void>();
+
+
+
+constructor(){
+  console.log('ChangePasswordComponent intialized with changePswd:',this.changePswd)
+  console.log('ChangePasswordComponent intialized with isChangePassword:',this.isChangePassword)
+  console.log('ChangePasswordComponent intialized with showPassword:',this.showPassword)
+  console.log('ChangePasswordComponent intialized with showConfirmPassword:',this.showConfirmPassword)
+  console.log('ChangePasswordComponent intialized with errorMsz:',this.errorMsz)
+  console.log('ChangePasswordComponent intialized with Edit:',this.Edit)
+  console.log('ChangePasswordComponent intialized with togglePassword:',this.togglePassword)
+  console.log('ChangePasswordComponent intialized with toggleConfirmPassword:',this.toggleConfirmPassword)
+  console.log('ChangePasswordComponent intialized with save:',this.save)
+  console.log('ChangePasswordComponent intialized with cancel:',this.cancel)
+}
+ngOnChanges(changes: SimpleChanges) 
+{
+  if(changes['changePswd'])
+  {
+    console.log('changePwd changed to',changes['changePswd'].currentValue);
+    this.changePswd = changes['changePswd'].currentValue;
+  }
+   if(changes['isChangePassword'])
+  {
+    console.log('isChangePassword changed to',changes['isChangePassword'].currentValue);
+    this.isChangePassword = changes['isChangePassword'].currentValue;
+  }
+   if(changes['showPassword'])
+  {
+    console.log('showPassword changed to',changes['showPassword'].currentValue);
+    this.showPassword = changes['showPassword'].currentValue;
+  }
+   if(changes['showConfirmPassword'])
+  {
+    console.log('showConfirmPassword changed to',changes['showConfirmPassword'].currentValue);
+    this.showConfirmPassword = changes['showConfirmPassword'].currentValue;
+  }
+   if(changes['errorMsz'])
+  {
+    console.log('errorMsz changed to',changes['errorMsz'].currentValue);
+    this.errorMsz = changes['errorMsz'].currentValue;
+  }
+   if(changes['Edit'])
+  {
+    console.log('Edit changed to',changes['Edit'].currentValue);
+    this.Edit = changes['Edit'].currentValue;
+  }
+   if(changes['togglePassword'])
+  {
+    console.log('togglePassword changed to',changes['togglePassword'].currentValue);
+    this.togglePassword = changes['togglePassword'].currentValue;
+  }
+   if(changes['toggleConfirmPassword'])
+  {
+    console.log('toggleConfirmPassword changed to',changes['toggleConfirmPassword'].currentValue);
+    this.toggleConfirmPassword = changes['toggleConfirmPassword'].currentValue;
+  }
+   if(changes['save'])
+  {
+    console.log('save changed to',changes['save'].currentValue);
+    this.save = changes['save'].currentValue;
+  }
+   if(changes['cancel'])
+  {
+    console.log('cancel changed to',changes['cancel'].currentValue);
+    this.cancel = changes['cancel'].currentValue;
+  }
+}
+}
--- BPM.Web.Dealer.UI/src/app/components/profile/user-personal-info.component.ts
+++ BPM.Web.Dealer.UI/src/app/components/profile/user-personal-info.component.ts
@@ -1,21 +1,69 @@
-import { Component, Input, SimpleChanges } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output, SimpleChanges } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-personal-info',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './user-personal-info.component.html',
   styleUrl: './user-personal-info.component.css',
 })
 export class UserPersonalInfoComponent {
   @Input() userId: string = '';
+  @Input() userPersionalSection: any = {};
+  @Input() isUserEditing: boolean = false;
+  @Input() errorMsz: string = '';
+
+  @Output() Edit = new EventEmitter<void>();
+  @Output() save = new EventEmitter<void>();
+  @Output() cancel = new EventEmitter<void>();
+
 
   constructor() {
     console.log('UserPersonalInfoComponent initialized with userId:', this.userId);
+    console.log('UserPersonalInfoComponent initialized with userPersionalSection:', this.userPersionalSection);
+    console.log('UserPersonalInfoComponent initialized with isUserEditing:', this.isUserEditing);
+    console.log('UserPersonalInfoComponent initialized with errorMsz:', this.errorMsz);
+    console.log('UserPersonalInfoComponent initialized with Edit event emitter:', this.Edit);
+    console.log('UserPersonalInfoComponent initialized with save event emitter:', this.save);
+    console.log('UserPersonalInfoComponent initialized with cancel event emitter:', this.cancel);
   }
   ngOnChanges(changes: SimpleChanges) {
     if (changes['userId']) {
       console.log('User ID changed to:', changes['userId'].currentValue);
       this.userId = changes['userId'].currentValue;
     }
+    if (changes['userPersionalSection']) {
+      console.log('User Data:', changes['userPersionalSection'].currentValue);
+      this.userPersionalSection = changes['userPersionalSection'].currentValue;
+    }
+    if(changes['isUserEditing'])
+    {
+      console.log('isUserEditing changed to:', changes['isUserEditing'].currentValue);
+      this.isUserEditing = changes['isUserEditing'].currentValue;
+    }
+    if(changes['errorMsz'])
+    {
+      console.log('errorMsz changed to:', changes['errorMsz'].currentValue);
+      this.errorMsz = changes['errorMessage'].currentValue;
+    }
+    if(changes['Edit'])
+    {
+      console.log('Edit event emitter changed to:', changes['Edit'].currentValue);
+      this.Edit = changes['Edit'].currentValue;
+    }
+    if(changes['save'])
+    {
+      console.log('save event emitter changed to:', changes['save'].currentValue);
+      this.save = changes['save'].currentValue;
+    }
+    if(changes['cancel'])
+    {
+      console.log('cancel event emitter changed to:', changes['cancel'].currentValue);
+      this.cancel = changes['cancel'].currentValue;
+    }
+
+    
   }
 }
+
