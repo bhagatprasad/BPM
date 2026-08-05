0b1fceb6eae77c903004749b8463f6c6af6a1e07
soyamraghu <soyamraghu65@gmail.com>
2026-08-05 15:48:50 +0530

    User_Component_Grid_Implementation

--- BPM.Web.Dealer.UI/src/app/components/user/user.component.ts
+++ BPM.Web.Dealer.UI/src/app/components/user/user.component.ts
@@ -1,19 +1,48 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectorRef, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { SpinnerLoadingService } from '@app/common/services/spinner-loading-service';
-import { UserService } from '@app/services/profile.service';
+import { userInformation } from '@app/models/user';
+import { UserDetailsService } from '@app/services/user.service';
 import { ToastrService } from '@iqx-limited/ngx-toastr';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
-export class UserComponent { 
-constructor(private userService: UserService ){}
+export class UserComponent {
+  userInformation: userInformation[] = [];
+  dealerId: string = '';
+  error: string | null = null;
+  userData: any;
+  userId: any;
+  constructor(private userService: UserDetailsService) { }
 
-
-
+  ngOnInit(): void {
+     const storedData = localStorage.getItem('AuthenticatedUserResponse');
+    if (storedData) {
+      this.userData = JSON.parse(storedData);
+      console.log('Full userData:', this.userData);
+      this.userId = this.userData.authenticateResponseDto.userId;
+      this.dealerId=this.userData.authenticateResponseDto.dealerId;
+    }
+    this.loadUsers();
+  }
+  loadUsers(): void {
+    
+    this.userService.getAllUsersByDealerId(this.dealerId).subscribe({
+      next: (response: userInformation[]) => {
+        console.log('Users fetched successfully:', response);
+        this.userInformation = response || [];
+      },
+      error: (error) => {
+        console.error('Error fetching users:', error);
+        this.error = 'Failed to load users. Please try again.';
+        this.userInformation = [];
+      }
+    });
+  }
 }
-  
--- BPM.Web.Dealer.UI/src/app/services/user.service.ts
+++ BPM.Web.Dealer.UI/src/app/services/user.service.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from '@app/common/services/api.service';
-import { usersRequest, UsersResponse } from '@app/models/user';
+import { userInformation, usersRequest,} from '@app/models/user';
 import { environment } from '@env/environment';
 import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root', })
 
-export class UserService {
+export class UserDetailsService {
 
     constructor(private apiService: ApiService) { }
 
-    getAllUsersByDealerId(dealerId: string, data: usersRequest): Observable<UsersResponse> {
-        const endponit = `${environment.UrlConstants.User.GetAllUsersByDealerIdAsync}/${dealerId}`;
-        return this.apiService.send<UsersResponse>('GET', endponit);
+    getAllUsersByDealerId(dealerId: string): Observable<userInformation[]> {
+        const endpoint = `${environment.UrlConstants.User.GetAllUsersByDealerIdAsync}/${dealerId}`;
+        return this.apiService.send<userInformation[]>('GET', endpoint);
     }
+    
 }
