5f44de9fcb91a60bf76928308cf2c362797e2980
bhagatprasad <bhagatprasad8@yahoo.com>
2026-08-12 21:37:49 +0530

    commit

--- BPM.Web.API/Repository/SalesOrderRepository.cs
+++ BPM.Web.API/Repository/SalesOrderRepository.cs
@@ -141,7 +141,7 @@ namespace BPM.Web.API.Repository
             return true;
         }
 
-       
+
         public async Task<SalesOrder> ProcessSalesOrderAsync(Guid salesOrderId, string status)
         {
             var salesOrder = await _dbContext.SalesOrders.FirstOrDefaultAsync(a => a.Id == salesOrderId);
@@ -155,13 +155,6 @@ namespace BPM.Web.API.Repository
 
             await _dbContext.SaveChangesAsync();
 
-            return salesOrder;
-        }
-
-        public async Task<SalesOrder> UpdateSalesOrderAsync(SalesOrder salesOrder)
-        {
-            _dbContext.SalesOrders.Update(salesOrder);
-            await _dbContext.SaveChangesAsync();
             return salesOrder;
         }
     }
