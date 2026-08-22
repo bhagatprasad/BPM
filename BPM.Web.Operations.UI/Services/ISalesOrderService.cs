ea5e469afb87677d6a727becdc39c0a3429c7679
bhagatprasad <bhagatprasad8@yahoo.com>
2026-08-22 22:23:24 +0530

    Now LoadSalesOrders() will be called immediately when the SalesOrderView is created, and the data will load properly!

---
+++ BPM.Web.Operations.UI/Services/ISalesOrderService.cs
@@ -0,0 +1,9 @@
+﻿using BPM.Web.Operations.UI.Models;
+
+namespace BPM.Web.Operations.UI.Services
+{
+    public interface ISalesOrderService
+    {
+        Task<IEnumerable<SalesOrderDto>> GetAllSalesOrderAsync();
+    }
+}
